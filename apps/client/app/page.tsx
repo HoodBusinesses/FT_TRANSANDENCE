a2d1d779a42a7c102aaa0543c835f03c09b55024
Yousra Ibarki <yoibarki@e3r7p10.1337.ma>
2024-09-12 18:25:57 +0100

    add: page to lunch the game added

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,5 +1,13 @@
-export default function Home() {
+"use client";
+
+import React from "react";
+import "./globals.css";
+import { GameHome } from "./LunchGame/Game.js";
+
+export default function Display() {
   return (
-    <>Hello, World!</>
+    <>
+      <GameHome />
+    </>
   );
 }
