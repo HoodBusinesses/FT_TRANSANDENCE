47c2beb4233dfdd2695c6daf8447a22734bfd0d1
Yousra Ibarki <yoibarki@e1r2p15.1337.ma>
2024-09-25 10:06:55 +0100

    carousel fixed

--- apps/client/app/HomeGame/Maps.js
+++ apps/client/app/HomeGame/Maps.js
@@ -5,90 +5,93 @@ import "slick-carousel/slick/slick-theme.css";
 import { useState } from "react";
 import Slider from "react-slick";
 
-function CenterMode() {
-  const [activeSlide, setActiveSlide] = useState(0);
 
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "1px",
-    slidesToShow: 3,
-    speed: 500,
-    beforeChange: (current, next) => setActiveSlide(next),
-  };
+import { useRef, memo } from 'react';
+import { StackedCarousel, ResponsiveContainer } from 'react-stacked-center-carousel';
 
+const data = [
+	{ cover: './table1.svg', title: 'Dunkirk' },
+	{ cover: './table1.svg', title: 'Dunkirk' },
+	{ cover: './table1.svg', title: 'Dunkirk' },
+	{ cover: './table1.svg', title: 'Dunkirk' },
+	{ cover: './table1.svg', title: 'Dunkirk' }
+  ];
+
+function ResponsiveCarousel() {
+  const ref = useRef({});
+  
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div
-          className={` slide ${activeSlide === 0 ? "active h-96" : "inactive "}`}
-        >
-          <h3>
-            {" "}
-            <img src="table1.svg" className="h-auto" />
-          </h3>
-        </div>
-        <div
-          className={`slide ${activeSlide === 1 ? "active h-96 " : "inactive"}`}
-        >
-          <h3>
-            {" "}
-            <img src="table1.svg" className="h-auto" />
-          </h3>
-        </div>
-        <div
-          className={`slide ${activeSlide === 2 ? "active h-96" : "inactive"}`}
-        >
-          <h3>
-            {" "}
-            <img src="table1.svg" className="h-auto" />
-          </h3>
-        </div>
-        <div
-          className={`slide ${activeSlide === 3 ? "active h-96" : "inactive"}`}
-        >
-          <h3>
-            {" "}
-            <img src="table1.svg" className="h-auto" />
-          </h3>
-        </div>
-        <div
-          className={`slide ${activeSlide === 4 ? "active h-96" : "inactive"}`}
-        >
-          <h3>
-            {" "}
-            <img src="table1.svg" className="h-auto" />
-          </h3>
-        </div>
-      </Slider>
+    <div style={{ width: '100%', position: 'relative' }}>
+      {/* ResponsiveContainer will have the same width as its parent element */}
+      <ResponsiveContainer
+        carouselRef={ref}
+        render={(parentWidth, carouselRef) => {
+          let currentVisibleSlide = 5;
+          if (parentWidth <= 1440) currentVisibleSlide = 3;
+          else if (parentWidth <= 1080) currentVisibleSlide = 1;
+
+          return (
+            <StackedCarousel
+              ref={carouselRef}
+              data={data}
+              carouselWidth={parentWidth}
+              slideWidth={500}
+              slideComponent={Card}
+              maxVisibleSlide={5}
+              currentVisibleSlide={currentVisibleSlide}
+              useGrabCursor={true}
+            />
+          );
+        }}
+      />
     </div>
   );
 }
 
-export default CenterMode;
+// Very important to memoize your component!!!
+const Card = memo(
+	function (props) {
+	  const { data, dataIndex } = props;
+	  const { cover } = data[dataIndex];
+  
+	  return (
+		<div style={{ width: '100%', height: 300 }}>
+		  <img
+			style={{ height: '100%', width: '100%', objectFit: 'cover', borderRadius: 10 }}
+			draggable={false}
+			src={cover}
+			alt="carousel-slide"
+		  />
+		</div>
+	  );
+	},
+	function (prevProps, nextProps) {
+	  return prevProps.dataIndex === nextProps.dataIndex;
+	}
+  );
+  
 
 export function Maps() {
   return (
     <div
-      className="min-h-[calc(100vh-103px)] min-w-full"
+      className="min-h-[calc(100vh-104px)] "
       style={{
         backgroundColor: "#222831",
         fontFamily: "Kaisei Decol",
         color: "#FFD369",
       }}
     >
-      <div className="">
+      <div className="a">
         <div>
-          <h1 className="text-2xl flex justify-center font-extralight pt-20 pb-5 tracking-widest">
+          <h1 className="text-2xl flex justify-center font-extralight pt-20 pb-10 tracking-widest">
             Maps
           </h1>
         </div>
-        <div className="">
-          <CenterMode />
+        <div className="mb-32">
+          <ResponsiveCarousel />
         </div>
         <div>
-          <h1 className="text-2xl flex justify-center font-extralight pb-5 tracking-widest">
+          <h1 className="text-2xl flex justify-center font-extralight pb-10 pt-10tracking-widest">
             Mode
           </h1>
         </div>
@@ -113,8 +116,7 @@ export function Maps() {
           </div>
         </div>
         <div className="flex justify-center pb-20">
-          <button onClick={() => (window.location.href = "#")} className="bg-[#393E46] p-5 rounded-[30px] w-[20%] border-2 ">
-            {" "}
+          <button onClick={() => (window.location.href = "#")} className="bg-[#393E46] p-5 m-24 rounded-[30px] w-48 border-2 ">
             <span className="text-2xl tracking-widest ">Play</span>{" "}
           </button>
         </div>
