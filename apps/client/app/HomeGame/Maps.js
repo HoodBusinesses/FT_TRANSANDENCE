83879206dc99812ebde81d0cf7e24974c69c72c0
Yousra Ibarki <yousra.ibarki@gmail.com>
2024-09-25 18:14:46 +0100

    fixing the light of buttons

--- apps/client/app/HomeGame/Maps.js
+++ apps/client/app/HomeGame/Maps.js
@@ -2,74 +2,7 @@ import React from "react";
 import "./../globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useState } from "react";
-import Slider from "react-slick";
-
-
-import { useRef, memo } from 'react';
-import { StackedCarousel, ResponsiveContainer } from 'react-stacked-center-carousel';
-
-const data = [
-	{ cover: './table1.svg', title: 'Dunkirk' },
-	{ cover: './table1.svg', title: 'Dunkirk' },
-	{ cover: './table1.svg', title: 'Dunkirk' },
-	{ cover: './table1.svg', title: 'Dunkirk' },
-	{ cover: './table1.svg', title: 'Dunkirk' }
-  ];
-
-function ResponsiveCarousel() {
-  const ref = useRef({});
-  
-  return (
-    <div style={{ width: '100%', position: 'relative' }}>
-      {/* ResponsiveContainer will have the same width as its parent element */}
-      <ResponsiveContainer
-        carouselRef={ref}
-        render={(parentWidth, carouselRef) => {
-          let currentVisibleSlide = 5;
-          if (parentWidth <= 1440) currentVisibleSlide = 3;
-          else if (parentWidth <= 1080) currentVisibleSlide = 1;
-
-          return (
-            <StackedCarousel
-              ref={carouselRef}
-              data={data}
-              carouselWidth={parentWidth}
-              slideWidth={500}
-              slideComponent={Card}
-              maxVisibleSlide={5}
-              currentVisibleSlide={currentVisibleSlide}
-              useGrabCursor={true}
-            />
-          );
-        }}
-      />
-    </div>
-  );
-}
-
-// Very important to memoize your component!!!
-const Card = memo(
-	function (props) {
-	  const { data, dataIndex } = props;
-	  const { cover } = data[dataIndex];
-  
-	  return (
-		<div style={{ width: '100%', height: 300 }}>
-		  <img
-			style={{ height: '100%', width: '100%', objectFit: 'cover', borderRadius: 10 }}
-			draggable={false}
-			src={cover}
-			alt="carousel-slide"
-		  />
-		</div>
-	  );
-	},
-	function (prevProps, nextProps) {
-	  return prevProps.dataIndex === nextProps.dataIndex;
-	}
-  );
-  
+import { ResponsiveCarousel } from "./Carousel";
 
 export function Maps() {
   return (
@@ -96,31 +29,29 @@ export function Maps() {
           </h1>
         </div>
         <div className="flex justify-center gap-10 mb-16">
-          <div>
-            <button
-              className="bg-[#393E46] p-7 rounded-lg w-48 relative"
-              onClick={() => (window.location.href = "#")}
-            >
-              <div className="w-4 h-4 bg-[#222831] rounded-full absolute top-2 right-2 " />
-              <span className="text-2xl tracking-widest"> Classic </span>
-            </button>
-          </div>
-          <div>
-            <button
-              className=" bg-[#393E46] p-7 rounded-lg w-48 relative"
-              onClick={() => (window.location.href = "#")}
-            >
-              <div className="w-4 h-4 bg-[#222831] rounded-full absolute top-2 right-2  " />
-              <span className="text-2xl tracking-widest"> Tournament </span>
-            </button>
-          </div>
+          <a
+            className="bg-[#393E46] p-7 rounded-lg w-48 text-center relative group"
+            href="./#"
+          >
+            <span className="w-4 h-4 rounded-full absolute top-2 right-2 transition-all bg-blue_dark group-hover:bg-golden" />
+            <span className="text-2xl tracking-widest"> Classic </span>
+          </a>
+          <a className=" bg-[#393E46] p-7 rounded-lg w-48 relative group" href="./#">
+            <span className="w-4 h-4 rounded-full absolute top-2 right-2 transition-all bg-blue_dark group-hover:bg-golden" />
+            <span className="text-2xl tracking-widest"> Tournament </span>
+          </a>
         </div>
         <div className="flex justify-center pb-20">
-          <button onClick={() => (window.location.href = "#")} className="bg-[#393E46] p-5 m-24 rounded-[30px] w-48 border-2 ">
+          <a
+            href="./#"
+            className="bg-[#393E46] p-5 m-24 rounded-[30px] w-48 border text-center "
+          >
             <span className="text-2xl tracking-widest ">Play</span>{" "}
-          </button>
+          </a>
         </div>
       </div>
     </div>
   );
 }
+
+// transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        golden: '#FFD369',
+        blue_dark: '#222831',
+      }
+    },
   },
   plugins: [],
 }
